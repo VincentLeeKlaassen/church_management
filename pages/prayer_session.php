e443f51c31c848627bc0af73438839c6992cb6c6
VincentLeeKlaassen <klaassenvl@gmail.com>
2024-10-23 11:04:44 +0200

    initial commit

---
+++ pages/prayer_session.php
@@ -0,0 +1,22 @@
+<?php require_once('../config.php'); ?>
+<!DOCTYPE HTML>
+<head>
+<title> Prayer Session | <?php echo $_settings->info('short_name') ?></title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="robots" content="index, follow">
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Prayer Session" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="http://localhost/church_management/uploads/blog_uploads/banners/4_banner.jpg" />
+<meta property="og:url" content="http://localhost/church_management/pages/prayer_session.php" />
+<?php require_once('../inc/page_header.php') ?>
+</head>
+<?php include(base_app.'inc/body_block.php') ?>
+<h2>Prayer Session</h2>
+<input name="blog_id" value="a87ff679a2f3e71d9181a67b7542122c" type="hidden">
+<hr>
+<small><span class='text-muted'><i class='fa fa-calendar-day'></i> Published: Jun 11,2024 09:09 AM</span></small>
+<center><img src="http://localhost/church_management/uploads/blog_uploads/banners/4_banner.jpg" class="img-thumbnail img-banner" alt="http://localhost/church_management/pages/prayer_session.php" /></center>
+
+<?php include(base_app.'inc/body_block_end.php') ?>
